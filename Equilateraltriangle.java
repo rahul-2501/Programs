8626892b6a21b56464b7af1a7caeeae0ae9ddaae
Rahul <38251726+rahul-2501@users.noreply.github.com>
2019-04-26 20:48:19 +0530

    Update Equilateraltriangle.java

--- Equilateraltriangle.java
+++ Equilateraltriangle.java
@@ -3,14 +3,14 @@ public class equitriangle {
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		
 		int ar=5;
 		for(int i=0;i<ar;i++)
 		{
 			for(int j=0;j<ar+i;j++)
 			{
 				int n=1+i;
-				if(j>=ar-n && j<=ar*10)
+				if(j>=ar-n)
 				{
 					System.out.print("*");
 				}
