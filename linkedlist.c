fdda3115c43961f7f43cc27ae827d198a062a04a
Rahul <38251726+rahul-2501@users.noreply.github.com>
2019-04-28 04:47:32 +0530

    Add files via upload

---
+++ linkedlist.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node{
+ int data;
+ struct node *next;
+}node;
+
+node *insert(int data);
+void display(node *head);
+node *head=NULL;
+int main()
+{
+node *h=NULL;
+h=insert(1);
+insert(2);
+insert(4);
+insert(5);
+display(h);
+
+return 0;
+}
+
+node *insert(int data)
+
+{
+   node *temp=NULL;
+   node *p=NULL;
+   temp=(node*)malloc(sizeof(node));
+   temp->data=data;
+   temp->next=NULL;
+
+if(head==NULL)
+{
+  head=temp;
+  head->next=NULL;
+}
+else
+{
+  p=head;
+  while(p->next!=NULL)
+{
+ p=p->next;
+
+}
+p->next=temp;
+}
+return head;
+}
+  
+
+void display(node *head)
+{
+  node *p=head;
+while(p->next!=NULL)
+{
+  printf("%d->",p->data);
+ p=p->next;
+}
+printf("%d",p->data);
+}
